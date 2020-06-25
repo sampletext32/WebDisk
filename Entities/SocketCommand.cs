0ef669fa495b45118cb9f3fd40f73a10c75e1168
sampletext32 <sampletext32@bk.ru>
2020-06-25 16:23:17 +0300

    Add Socket Command

---
+++ Entities/SocketCommand.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public abstract class SocketCommand
+    {
+        protected object Data { get; set; }
+
+        public byte[] Serialize()
+        {
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(ms, this);
+            byte[] data = ms.ToArray();
+            return data;
+        }
+
+        public static SocketCommand Deserialize(byte[] data)
+        {
+            MemoryStream ms = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+            SocketCommand command = (SocketCommand) formatter.Deserialize(ms);
+            return command;
+        }
+
+        protected SocketCommand(object data)
+        {
+            Data = data;
+        }
+    }
+
+    [Serializable]
+    public class EmptyCommand : SocketCommand
+    {
+        public EmptyCommand() : base(null)
+        {
+        }
+    }
+
+
+    [Serializable]
+    public class HelloCommand : SocketCommand
+    {
+        public string GetData()
+        {
+            return (string) Data;
+        }
+
+        public HelloCommand(object data) : base(data)
+        {
+        }
+    }
+
+    [Serializable]
+    public class GetFolderHtmlCommand : SocketCommand
+    {
+        public string GetData()
+        {
+            return (string) Data;
+        }
+
+        public GetFolderHtmlCommand(string data) : base(data)
+        {
+        }
+    }
+
+    [Serializable]
+    public class CompareHashCommand : SocketCommand
+    {
+        public string GetData()
+        {
+            return (string) Data;
+        }
+
+        public CompareHashCommand(string data) : base(data)
+        {
+        }
+    }
+
+    [Serializable]
+    public class ResponseCompareHashCommand : SocketCommand
+    {
+        public bool GetData()
+        {
+            return (bool) Data;
+        }
+
+        public ResponseCompareHashCommand(bool data) : base(data)
+        {
+        }
+    }
+}
