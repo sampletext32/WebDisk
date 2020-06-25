f22353b7ce36137c18d8f6f2a9280fc8e859a6b9
sampletext32 <sampletext32@bk.ru>
2020-06-25 14:36:55 +0300

    Add basic client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,10 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            SocketHandler handler = new SocketHandler(IPAddress.Loopback, 11771);
+            handler.Init();
+            handler.Connect();
+            Console.ReadKey();
         }
     }
 }
---
+++ Client/SocketHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class SocketHandler
+    {
+        private IPAddress _ipAddress;
+        public int _port;
+        private Socket _socket;
+        private byte[] _buffer;
+
+        private IAsyncResult _connectResult;
+
+        public SocketHandler(IPAddress ipAddress, int port)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        public void Init()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect()
+        {
+            _connectResult = _socket.BeginConnect(_ipAddress, _port, OnConnect, null);
+        }
+
+        private void OnConnect(IAsyncResult ar)
+        {
+            _socket.EndConnect(_connectResult);
+
+            string testData = "Hello, i am client";
+            var bytes = Encoding.UTF8.GetBytes(testData);
+            //TODO: Send data size
+            _socket.Send(BitConverter.GetBytes(bytes.Length), 0, 4, SocketFlags.None);
+            //TODO: Send request
+            _socket.Send(bytes, 0, bytes.Length, SocketFlags.None);
+
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+    }
+}
