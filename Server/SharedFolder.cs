4f5456d56a6bf275f5c94373e7282e1294f2aa38
sampletext32 <sampletext32@bk.ru>
2020-06-25 16:25:23 +0300

    Add SharedFolder

---
+++ Server/SharedFolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.TreeNodes;
+
+namespace Server
+{
+    public class SharedFolder
+    {
+        private string _path;
+        private string _hash;
+
+        public SharedFolder(string path)
+        {
+            _path = path;
+        }
+
+        public TreeNode AsTreeNode()
+        {
+            TreeAnalyzer analyzer = new TreeAnalyzer(_path);
+            analyzer.Retrieve();
+            var treeNode = analyzer.GetTree();
+            return treeNode;
+        }
+    }
+}
