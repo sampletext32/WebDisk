7d693780eb3dd2fd4a8231942e07282a4bf4b1df
sampletext32 <sampletext32@bk.ru>
2020-06-25 16:25:32 +0300

    Add command Handlers

---
+++ Client/ClientCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Client
+{
+    public class ClientCommandHandler
+    {
+        public static object Handle(SocketCommand command)
+        {
+            if (command is ResponseCompareHashCommand responseCompareHashCommand)
+            {
+                var foldersEquals = responseCompareHashCommand.GetData();
+                Console.WriteLine("FoldersEquals: {0}", foldersEquals);
+                return foldersEquals;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Server/ServerCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Server
+{
+    public class ServerCommandHandler
+    {
+        private static SharedFolder _sharedFolder = new SharedFolder("C:\\Projects\\CSharp\\WebDisk\\Server\\bin\\Debug\\shared");
+
+        public static SocketCommand Handle(SocketCommand command)
+        {
+            if (command is HelloCommand helloCommand)
+            {
+                Console.WriteLine(helloCommand.GetData());
+                return new EmptyCommand();
+            }
+            else if (command is CompareHashCommand compareHashCommand)
+            {
+                var treeEquals = _sharedFolder.AsTreeNode().GetHash() == compareHashCommand.GetData();
+                ResponseCompareHashCommand responseCompareCommand = new ResponseCompareHashCommand(treeEquals);
+                return responseCompareCommand;
+            }
+            return new EmptyCommand();
+        }
+    }
+}
