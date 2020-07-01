b8b0fed0335b290a6614f56c55e5410f770ab90c
sampletext32 <sampletext32@bk.ru>
2020-07-01 16:42:45 +0300

    Add Consant

--- Entities/Constants.cs
+++ Entities/Constants.cs
@@ -6,6 +6,7 @@
         public const int ConnectionPort = 11771; // Default connection port
         public const int ConnectionTimeoutMilliseconds = 100; // Default client connection timeout
         public const int ServerMaximumBacklogSockets = 100; // Default waiting sockets amount
+        public const int ClientSynchronizationDelayMilliseconds = 10_000; // Default client sync delay
 
         public const bool Debug = false; // Are programs required to print every step to console
     }
